f72a5d7b30a36cb7ae53bdfe4742531b5dcc3e1b
danielyangfei <1243702693@qq.com>
2025-04-29 18:04:01 +0800

    0.0.1

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'New Coder',
+    description: 'This is blog for new coder for no backgroud knowledge like product manager, designer,and operator.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
+  /*  socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
       nuxt: {
@@ -19,7 +19,7 @@ export default defineAppConfig({
       repo: 'docus',
       owner: 'nuxt-themes',
       edit: true
-    },
+    },*/
     aside: {
       level: 0,
       collapsed: false,
